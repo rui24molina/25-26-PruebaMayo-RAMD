d671d6f95d4e0690255a8ee854213d5864322c4d
rui <rui@rui.rui>
2026-05-07 18:15:12 +0200

    error de linting

--- app/main.py
+++ app/main.py
@@ -7,6 +7,10 @@ Base.metadata.create_all(bind=engine)
 app = FastAPI()
 app.include_router(items.router)
 
+#prueba linting
+print(variable) 
+    error = "esto esta mal"
+
 # Código que se añade
 @app.get("/status")
 def version():
